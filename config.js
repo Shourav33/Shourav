f3366f4e185a78c7dfd763720de9a09ec847cfbd
mohiburshourav33@gmail.com <mohiburshourav33@gmail.com>
2023-02-28 20:09:41 +0600

    Fast test

---
+++ config.js
@@ -0,0 +1,97 @@
+        //js//
+
+var man = "12";
+console.log(man);
+
+//String//
+
+var strType = "Mohibur Rohaman"
+console.log(typeof strType);
+
+//Number//
+
+var numType = 40;
+console.log(typeof numType);
+
+//toUpperCase//
+
+var rochona = "age home year four people five get held the get khan home five get get home";
+console.log(rochona.toUpperCase());
+
+//toLowerCase//
+
+var rochona = "AGE HOME YEAR FOUR PEOPLE FIVE GET HELD THE GET KHAN HOME FIVE GET GET HOME";
+console.log(rochona.toLowerCase());
+
+//Split//
+
+var myNu = "Shourav Lice Rahim Tonmoy";
+console.log(myNu.split("Lice"))
+
+//indexOF//
+
+var myNu = "Shourav Lice Rahim Tonmoy";
+console.log(myNu.indexOf("Lice"))
+
+
+var num1 = 20;
+var num2 = 40;
+
+var total = num1 + num2;
+console.log(total)
+
+
+var num1 = "20";
+var num2 = 40;
+
+num1 = parseFloat(num1)
+console.log(num1 + num2)
+
+var man = 40 ;
+var women = 30 ;
+
+console.log(man-women)
+
+var man = 40;
+var women = 3;
+
+console.log(man * women)
+
+var man = 40;
+var women = 4;
+
+console.log(man / women)
+
+var man = 40;
+var women = 30;
+
+console.log(man % women)
+
+var nam1 = -20 ;
+var all = Math.abs (nam1)
+
+console.log(all)
+
+var nam1 = 20 ;
+var nam2 = 0.6 ;
+var all = Math.round(nam2)
+
+console.log(all)
+
+var nam1 = 10 ;
+var nam2 = 0.00001 ;
+var all = Math.ceil(nam2)
+
+console.log(all)
+
+var nam1 = 10 ;
+var nam2 = 0.7 ;
+var all = Math.floor(nam2)
+
+console.log(all)
+
+var  nam1 =10 ;
+var  nam2 = Math.random()
+
+console.log(nam2)
+
